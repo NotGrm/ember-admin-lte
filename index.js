9e1a2a4f28b007ab9d79bf188fe955e2712db6e7
NotGrm <notgrm@gmail.com>
2016-07-04 23:38:17 +0200

    Add admin-lte css to ember-cli build

--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-admin-lte'
+  name: 'ember-admin-lte',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/admin-lte/dist/css/AdminLTE.min.css');
+    app.import(app.bowerDirectory + '/admin-lte/dist/css/skins/_all-skins.min.css');
+  },
+
+  isDevelopingAddon: function() {
+    return true;
+  }
 };
